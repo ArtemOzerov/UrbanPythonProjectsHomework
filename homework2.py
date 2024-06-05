dd331925485d0b64528d8397a2ff30ec61dcbd8c
ArtemOzerov <tigger2009@mail.ru>
2024-06-05 18:57:18 +0300

    дз
    дз

---
+++ homework2.py
@@ -0,0 +1,26 @@
+выполнено_ДЗ = 12
+затрачено_часов = 1.5
+Курс = "Python"
+время_на_1_задание = 7.5
+print("Курс:", Курс,",всего задач:", выполнено_ДЗ,",затрачено часов:",затрачено_часов,",среднее время выполнения:",время_на_1_задание )
+
+
+hack = 'взлом пентагона'
+print(hack, "1%")
+print(hack, "5%")
+print(hack, "10%")
+print(hack, "15%")
+print(hack, "20%")
+print(hack, "25%")
+print(hack, "30%")
+print(hack, "35%")
+print(hack, "40%")
+print(hack, "45%")
+print(hack, "50%")
+print(hack, "55%")
+print(hack, "60%")
+print(hack, "70%")
+print(hack, "80%")
+print(hack, "90%")
+print(hack, "99%")
+print(hack, "100%")

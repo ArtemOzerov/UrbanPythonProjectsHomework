0bef6c4a5e8596225c1fe2798bfa76194ef91679
ArtemOzerov <tigger2009@mail.ru>
2024-06-07 22:23:33 +0300

    дз
    дз

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var = 2, True
+print(immutable_var)
+#кортеж – это неизменяемая упорядоченная коллекция, которая может содержать в себе разные типы данных.
+mutable_list = [5, False]
+mutable_list[0] = 12
+print(mutable_list)

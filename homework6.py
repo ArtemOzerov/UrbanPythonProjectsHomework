b44e67a21b34c4660c8d36b1fc651e0a2c17d186
ArtemOzerov <tigger2009@mail.ru>
2024-06-08 19:32:04 +0300

    дз
    дз

---
+++ homework6.py
@@ -0,0 +1,16 @@
+my_dict = {"Artem": 2009, "Alex": 1973}
+print(my_dict)
+print(my_dict["Artem"])
+my_dict["Abdullah"] = 1945
+print(my_dict["Abdullah"])
+my_dict.update({"Omar": 1999,
+                "Dima": 1991})
+a = my_dict.pop('Omar')
+print(a)
+my_set = {627, 544, 965, 544, True, True}
+print(my_set)
+my_set.add(5)
+my_set.add(6)
+print(my_set)
+my_set.remove(6)
+print(my_set)

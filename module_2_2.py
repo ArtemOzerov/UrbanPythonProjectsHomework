1b56e5dfa8a27e22798527d8a078b9f13a27cab6
ArtemOzerov <tigger2009@mail.ru>
2024-06-12 17:35:38 +0300

    дз
    дз

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+print("Введите 3 числа:")
+first = int(input('1: '))
+second = int(input('2: '))
+third = int(input('3: '))
+if first == second and first == third and second == third:
+    print('3 одинаковых числа')
+elif first == second or first == third or second == third:
+    print('2 одинаковых числа')
+else:
+    print('Одинаковых чисел нет')

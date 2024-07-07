3ef0f52cb1ce4fade4df98a9e6ce588555d2fe8e
ArtemOzerov <tigger2009@mail.ru>
2024-07-07 18:02:19 +0300

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -1,17 +1,18 @@
 numbers = range(1, 16)
-primes = []
-not_primes = []
-
+primes = []  # [2, 3, 5, 7, 11, 13]
+not_primes = []  # [4, 6, 8, 9, 10, 12, 14, 15]
+# is_prime = True  # после проверки (True - в prime, False - в not_prime).
 for z in numbers:
-    prime = True
-    for z in range(2, v):
-        if v % z == 0:
-            prime = False
+    if z == 1:
+        continue
+    is_prime = True
+    for v in range(2, z):
+        if z % v == 0:
+            is_prime = False
             break
-    if prime:
-        primes.append(v)
+    if is_prime:
+        primes.append(z)
     else:
-        not_primes.append(v)
-
-print('Простые:', primes)
-print('Не простые:', not_primes)
+        not_primes.append(z)
+print(primes)
+print(not_primes)

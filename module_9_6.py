ca28617f5e5b4d8bc37d0b00a55835f50afeb3a7
ArtemOzerov <tigger2009@mail.ru>
2024-08-31 19:46:57 +0300

    Add files via upload

---
+++ module_9_6.py
@@ -0,0 +1,21 @@
+def all_variants(text):
+    for size in range(1, len(text)+1):
+        for i in range(len(text)):
+            result = text[i]
+            if size > 1:
+                for j in range(len(text)):
+                    if j <= i:
+                        continue
+                    if not text[j] in result:
+                        result += text[j]
+                    if len(result) == size:
+                        yield result
+                        result = text[i]
+            else:
+                yield result
+def main():
+    a = all_variants("abc")
+    for i in a:
+        print(i)
+if __name__ == '__main__':
+    main()
